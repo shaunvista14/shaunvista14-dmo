74f95868a135bbf30106759b367a4730adcc2824
Shaun <vdmerweshaunanver@gmail.com>
2023-01-05 09:56:54 +0200

    Updated the Terms and Conditions

---
+++ pp.php
@@ -0,0 +1,110 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+<?php include 'includes/navbar.php'; ?>
+	 
+<div class="content-wrapper">
+<div class="container">
+
+
+<!-- Main content -->
+<section class="content">
+<div class="row">
+<div class="col-sm-9">
+
+<h1 class="page-header">Privacy Policy</h1>
+<p><a href="https://www.ioshopping.co.za">IOShopping.co.za</a> Privacy Policy has been compiled to reflect that of the Protection of Personal Information Act also known as the <a href="https://popia.co.za/">POPIA</a>  Act. The Act applies to any person or organisation who keeps any type of records relating to the personal information of anyone, unless those records are subject to other legislation which protects such information for example the Personally Identifiable Information (PII) Act and <a href="https://www.popiact-compliance.co.za/gdpr-information/31-lessons-learnt-from-gdpr">General Data Protection Regulations (GDPR)</a>. The <a href="#">Electronics Communications and Transactions ACT,</a> 2002 (ECT) provides for the facilitation and regulation of Electronic Communications and Transactions.</p>
+
+<p>It therefore sets the minimum standards for the <a href="https://popia.co.za/section-1-definitions/">protection of personal information</a>. It regulates the “processing” of personal information. “Processing” includes collecting, receiving, recording, organizing, retrieving, or using such information; or disseminating, distributing or making such personal information available.  Refer to Section 2, <a href="https://popia.co.za/section-2-purpose-of-act/">"Purpose of Act"</a> The Act will also relate to records which are already in the possession of the entity or person doing the processing. YOU are therefore advised to review the privacy policy carefully to better understand how <a href="https://www.ioshopping.co.za">IOShopping.co.za</a> collects, utilizes and protects YOUR Personal Information collected on our website.
+</p>
+
+
+<h1 class="page-header">Type of Personal Information collected</h1>
+<p><a href="https://www.ioshopping.co.za">IOShopping.co.za</a> collects personal information primarily to provide better service to all of our customers. When you visit our websites landingpage, some information is automatically collected through third-party tracking applications. This may include information such as the device Operating System (OS), Internet Protocol (IP) address, browser type etc. WE sometimes aggregate similar information from other customers to improve our Quality of Service offerings.</p>
+
+<p>We collect your Fistname and Surname, address details, email address and contact information when you sign up on our website preparing to place an order, or via our what's app calaogue. Otherwise, we do not collect users’ personal information for distributing to third-parties. We will retain your information as long as we require to provide you with the products and services and for such a period as mandated by the concerned law. If you opt to receive marketing correspondence from us, subscribe to our mailing list or newsletters, enter into any of our competitions or provide us with your details at networking events, we may use your personal data for our legitimate interest in order to provide you with details about our products services, business updates and events.</p>
+
+<h1 class="page-header">Collecting of Personal Information?</h1>
+<p>YOUR Personal Information is collected when you register on our website, place an order, subscribe to a newsletter, print media, respond to a survey, fill out a form, use live chat, engage on social media platforms such as <a href="https://www.facebook.com/IOShopping-109509931623158">Facebook</a>, <a href="https://www.instagram.com/ioshopping.ubuy/">Instagram</a>, <a href="https://twitter.com/io_shopping?t=iGBnb5KgwcZkRvgDwoXiiA&s=09">Twitter</a>, <a href="https://wa.me/message/XOWFFX4ZPFDAVH1">What's App Catalogue</a> and soon to be <a href="https://www.pinterest.com">Pinterest</a>, open a support ticket, call our support team for queries, provide us with feedback on our products or services on our website.</p>
+
+<h1 class="page-header">How your Personal Information is ultilized?</h1>
+<p>We use the information we collect primarily to improve our current products and services offerings:</p>
+
+<li>Personalize your experience and allow us to deliver the type of content and product offerings in which you are most interested.</li>
+<li>Deliver products and services you may request.</li>
+<li>Provide customer support, comments and enquiries.</li>
+<li>Process and complete your transactions.</li>
+<li>Send you related information, including confirmations, invoices, technical notices, updates, security alerts and support and administrative messages.</li>
+<li>Communicate with you about promotions, upcoming events and news about products and services.</li>
+<li>We may process your personal information without your knowledge or consent where required by applicable law or regulation for the purposes of verification of identity or for prevention, detection or investigation, including cyber incidents, prosecution and punishment of offences.</li>
+<li>Protect and investigate against fraudulent, unauthorized or illegal activity.</li>
+<li>Ask for rating and review for our products and services.</li>
+
+<h1 class="page-header">Customer Rights</h1>
+<p>Under applicable law, you have the following rights while accessing the website and what's app catalogue.
+</p>
+<li>You have the right to request a copy of the personal information we hold about you.
+<li>We want to have accurate information about you. Please contact us at info@ubuy.com if you think that the personal information we collect is not up to date or correct, we will update it for you.
+<li>You have the right to ask us to delete your personal information by opting 'OUT" from any marketing material you received. 
+<LI>In addition, where we use your personal information on legitimate interest grounds then, if you ask us to, we will stop using that information unless there are overriding legitimate grounds to continue. <strong>NB:</strong> that this will not affect any processing that has already taken place.</LI>
+
+
+<h1 class="page-header">Cyber Security</h1>
+<p>Protecting your Personal Information while browsing our <a href="https://www.ioshopping.co.za">IOShopping.co.za</a> is of upmost importance. Your personal information is processed behind secured networks and is only accessible by a limited number of persons who have special access rights to such systems, and are required to keep the information confidential. All sensitive information you supply is encrypted via Secure Sockets Layer (SSL) technology sepcially when accessesing the website to place an order, enter or submittion of Personal Information; all this information is protected with this technology. <strong>NB</strong> All transactions are processed through a Secure Gateway Provider and are not stored or processed on our servers.</p>
+
+<h1 class="page-header">What are Cookies</h1>
+<p>Cookies are small files that a website or its Service Provider transfers to your device name desktop computer of laptops hard drive via your Web browser once allowed. This enables the website's or service provider's systems to recognize your browser, capture and store certain information. For example, we use cookies to help us remember and process the items in your shopping cart. They are also used to help us understand your preferences based on previous or current website activity, which enables us to provide you with improved services.</p>
+
+<h1 class="page-header">What are Cookies used for?</h1>
+<li>Personalization and website accessibility.</li>
+<li>Understand and save your preferences for future visits.</li>
+<li>Keep track of advertisements.</li>
+<li>Track the site traffic in order to offer a better accessibility solution to the users.</li>
+<li>Trusted third-party services that track this information on our behalf.</li>
+<li><strong>NB:</strong> You can choose to have your computer notify you each time when a cookie is being sent or you can choose to turn off all cookies.You do this through your browser settings.You can check the ‘Help’ menu to learn the correct way to modify your cookies. If you turn cookies off it will turn off some of the features of the site.
+
+
+<h1 class="page-header">Country Consumer Rights</h1>
+<p>Refer to the relevants Acts Above governing consumer right in their respective countries. 
+Do We Disclose the Information We Collect to Third-Parties? We transfer to outside parties (like shipping companies, payment gateways) your name, address, city, town, phone number, email address. Payment gateways will also collect your card information if you will choose to make an online payment using a Credit, Debit card. We engage in this practice because we have a contract with third parties (to whomever we transfer your data) so all your data will be disposed of at their end after a certain time.</p>
+
+<h1 class="page-header">Third-Party Links</h1>
+<p>Occasionally, at our discretion, we may include or offer third-party products or services on our website and what's app catalogue. These third-party sites have separate and independent privacy policies. We, therefore, have no responsibility or liability for the content and activities of these linked sites. Nonetheless, we seek to protect the integrity of our website and welcome any feedback about these websites.</p>
+
+<h5><strong>Google</strong></h5>
+<p>Google's advertising requirements can be summed up by Google's Advertising Principles. They are put in place to provide a positive experience for users. We use Google AdSense Advertising on our website Google, as a third-party vendor, uses cookies to serve ads on our site. We adhere to the Google Ads policy as mentioned in this <a href="https://support.google.com/adspolicy/answer/6008942?hl=en&visit_id=637729797749944249-3941641082&rd=1">link</a>  
+Google's use of the DART cookie enables it to serve ads to our users based on previous visits to our site and other sites on the Internet. Users may opt-out of the use of the DART cookie by visiting the Google Ads and Content Network privacy policy.</p>
+
+<h5><strong>Social Media</strong></h5> 
+<p>We perform social media advertising to keep the users updated regarding the products and services. We do this by sharing media files and relevant content on our social media accounts like <a href="https://www.facebook.com/IOShopping-109509931623158">Facbook</a>, <a href="https://www.instagram.com/ioshopping.ubuy">Instagram</a>, <a href="https://twitter.com/io_shopping?t=iGBnb5KgwcZkRvgDwoXiiA&s=09">Twitter</a>, <a href="https://wa.me/message/XOWFFX4ZPFDAVH1">What's App Catalogue</a> and soon to be <a href="#">Pinterest</a>. A user may see an ad or the content that is provided on social media channels. We strictly follow the terms of use and privacy policies of the social media channels that we use. 
+/*We have implemented the following
+Remarketing with Google AdSense and Facebook
+Google Display Network Impression Reporting
+Demographics and Interests Reporting
+DoubleClick Platform Integration*/
+We, along with third-party vendors such as Google, use first-party cookies (such as the Google Analytics cookies) and third-party cookies (such as the DoubleClick cookies) or other third-party identifiers together to compile data regarding user interactions with ad impressions and other ad service functions as they relate to our website.</p>
+
+<h5><strong>Childrens Privacy</strong></h5>
+<p>Protecting the privacy of young children is especially important. For that reason, we do not collect or solicit personal information from anyone under the age of 13. In the event that we learn that we have collected personal information from a child under age 13, we will delete the information we have stored as quickly as possible from our website/app. If you believe that we might have any information from or about a child under 13, please contact us <a href="contacts.php">here</a>.</p>
+
+<h1 class="page-header">Changes to this Policy</h1>
+We may change this policy from time to time. You agree that your continued use of our services after such changes have been published to our services will constitute your acceptance of such revised policy.</p>
+<br></br>
+
+<h1><a href="index.php"><i class="fa fa-home"></a></i></h1>
+
+</div>
+		     
+</div>
+</div>
+<?php $pdo->close(); ?>
+<?php include 'includes/footer.php'; ?>
+</div>
+
+<?php include 'includes/analytics.php'; ?>
+<?php include 'includes/scripts.php'; ?>
+
+</body>
+</html>
