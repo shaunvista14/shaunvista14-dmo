e9dad717707dbaf702e498d0c37c7e8400716e84
Shaun <vdmerweshaunanver@gmail.com>
2023-01-05 14:54:15 +0200

    Removed code that was commented-out

---
+++ careers.php
@@ -0,0 +1,99 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+<?php include 'includes/navbar.php'; ?>
+	 
+<div class="content-wrapper">
+<div class="container">
+
+
+<!-- Main content -->
+<section class="content">
+<div class="row">
+<div class="col-sm-9">
+
+<h1 class="page-header">Careers</h1>
+
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+<ol class="carousel-indicators">
+<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+		
+<li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
+		
+<li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
+
+</ol>
+<div class="carousel-inner">
+<div class="item active">
+<img src="images/hr1.jpg" alt="First slide">
+</div>
+<div class="item">
+<img src="images/hr2.jpg" alt="Second slide">
+</div>
+
+</div>
+<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+<span class="fa fa-angle-left"></span>
+</a>
+<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+
+<span class="fa fa-angle-right"></span>
+		                </a>
+		            </div>
+
+<h1 class="page-header">We are Hiring!</h1>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magn
+	a aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <br><br>Email Cv's to: <a href="mailto:careers@ioshopping.co.za?subject=JOBID-014&nbsp;Web Developers Required!"> Human Resources</a>&nbsp;before 31st January 2023 </p>
+
+<ul>
+	<li>
+	<p><strong>UI/UX Designers</strong></p>
+	</li>
+	<li>
+	<p><strong>Frontend Developers</strong></p>
+	</li>
+	<li>
+	<p><strong>Backend Developers</strong></p>
+	</li>
+	<li>
+	<p><strong>FullStack Developers</strong></p>
+	</li>
+	<li>
+	<p><strong>Database Engineers</strong></p>
+	</li>
+
+	<ul>
+		<li>
+		<p>MySQL</p>
+		</li>
+		<li>
+		<p>SQL</p>
+		</li>
+		<li>
+		<p>Postgres SQL.</p>
+		</li>
+	</ul>
+	</li>
+</ul>
+
+<!--<p><a href="#"><strong><h3>Finibus Bonorum et Malorum</h3></strong></a></p>-->
+<h1 class="page-header">&nbsp; Finibus Bonorum et Malorum </h1> 
+<p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?" </p>
+<br></br>
+
+<h1><a href="index.php"><i class="fa fa-home"></a></i></h1>
+</div>
+		     
+</div>
+</div>
+<?php $pdo->close(); ?>
+<?php include 'includes/footer.php'; ?>
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+
+</body>
+</html>
